a1c6fd31175fc31d6be426ec0aac3b1a65004a40
Grant Barnes <gfbarnes78@gmail.com>
2022-01-23 15:15:42 -0600

    adding defaults and constraints to database

--- db/setup.sql
+++ db/setup.sql
@@ -8,61 +8,68 @@ USE tractor_pulling;
 
 CREATE TABLE tractors (
     id CHAR(36) NOT NULL,
-    brand VARCHAR(50) NOT NULL,
-    model VARCHAR(50),
-    PRIMARY KEY (id)
+    brand VARCHAR(50) DEFAULT "",
+    model VARCHAR(50) DEFAULT "",
+    PRIMARY KEY (id),
+    CONSTRAINT tractor_unique UNIQUE (brand, model)
 );
 
 CREATE TABLE pullers (
     id CHAR(36) NOT NULL,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    PRIMARY KEY (id)
+    first_name VARCHAR(50) DEFAULT "",
+    last_name VARCHAR(50) DEFAULT "",
+    PRIMARY KEY (id),
+    CONSTRAINT puller_unique UNIQUE (first_name, last_name)
 );
 
 CREATE TABLE locations (
     id CHAR(36) NOT NULL,
-    town VARCHAR(20) NOT NULL,
-    state CHAR(2) NOT NULL,
-    PRIMARY KEY (id)
+    town VARCHAR(20) DEFAULT "",
+    state CHAR(2) DEFAULT "",
+    PRIMARY KEY (id),
+    CONSTRAINT location_unique UNIQUE (town, state)
 );
 
 CREATE TABLE seasons (
     id CHAR(36) NOT NULL,
-    year CHAR(4) NOT NULL,
-    PRIMARY KEY (id)
+    year CHAR(4) DEFAULT "",
+    PRIMARY KEY (id),
+    CONSTRAINT season_unique UNIQUE (year)
 );
 
 CREATE TABLE pulls (
     id CHAR(36) NOT NULL,
     season CHAR(36) NOT NULL,
     location CHAR(36) NOT NULL,
-    date DATE,
-    youtube CHAR(11),
+    date DATE DEFAULT NOW(),
+    youtube CHAR(11) DEFAULT "",
     PRIMARY KEY (id),
     FOREIGN KEY (season) REFERENCES seasons(id),
-    FOREIGN KEY (location) REFERENCES locations(id)
+    FOREIGN KEY (location) REFERENCES locations(id),
+    CONSTRAINT pull_unique UNIQUE (season, location, date)
 );
 
 CREATE TABLE classes (
     id CHAR(36) NOT NULL,
     pull CHAR(36) NOT NULL,
-    category VARCHAR(16),
-    weight INT,
-    speed INT,
+    category VARCHAR(16) DEFAULT "",
+    weight INT DEFAULT 0,
+    speed INT DEFAULT 3,
     PRIMARY KEY (id),
-    FOREIGN KEY (pull) REFERENCES pulls(id)
+    FOREIGN KEY (pull) REFERENCES pulls(id),
+    CONSTRAINT class_unique UNIQUE (pull, category, weight, speed)
 );
 
 CREATE TABLE hooks (
     id CHAR(36) NOT NULL,
-    class CHAR(36),
-    puller CHAR(36),
-    tractor CHAR(36),
-    distance FLOAT(5, 2),
-    position INT,
+    class CHAR(36) NOT NULL,
+    puller CHAR(36) NOT NULL,
+    tractor CHAR(36) NOT NULL,
+    distance FLOAT(5, 2) DEFAULT 0.00,
+    position INT DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY (class) REFERENCES classes(id),
     FOREIGN KEY (puller) REFERENCES pullers(id),
-    FOREIGN KEY (tractor) REFERENCES tractors(id)
+    FOREIGN KEY (tractor) REFERENCES tractors(id),
+    CONSTRAINT hook_unique UNIQUE (class, puller, tractor, distance)
 );
